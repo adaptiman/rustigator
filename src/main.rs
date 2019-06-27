5c61d4c9492a6ea48236c0431e2bcb8bf40b28ff
adaptiman <sweeney@tamu.edu>
2019-06-27 14:54:06 -0500

    pushed to  migrate.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate rss;
-use rss::Channel;
+use rss::{Channel, Item};
 
 fn main() {
-    let channel = Channel::from_url("http://feeds.washingtonpost.com/rss/rss_morning-mix").unwrap();
-    println!("{:#?}",channel);
+    //reads the rss from URL and creates an rss::Channel
+    let mut channel = Channel::from_url("http://feeds.washingtonpost.com/rss/rss_morning-mix").unwrap();
+    //println!("{:#?}",channel);
+
+    //consumes the channel and returns a vector of Items
+    channel.set_items(vec![rss::Item::default()]);
+    assert_eq!(channel.into_items().len(), 1);
+    println!("{:#?}", channel);
+
 }
