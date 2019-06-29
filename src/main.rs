4092629e044a18f832de46cc2f32b2f0640680ba
llamicron <llamicron@gmail.com>
2019-06-28 23:40:48 -0500

    Adds code to parse rss feed into urls that match search terms

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,52 @@
 extern crate rss;
 use rss::{Channel, Item};
 
-fn main() {
-    //reads the rss from URL and creates an rss::Channel
-    let mut channel = Channel::from_url("http://feeds.washingtonpost.com/rss/rss_morning-mix").unwrap();
-    //println!("{:#?}",channel);
+fn search_terms() -> Vec<&'static str> {
+    let search_terms: Vec<&str> = vec![
+        "TRUMP",
+        "KOREA"
+    ];
+    search_terms
+}
 
-    //consumes the channel and returns a vector of Items
-    channel.set_items(vec![rss::Item::default()]);
-    assert_eq!(channel.into_items().len(), 1);
-    println!("{:#?}", channel);
+pub fn main() {
+    // reads the rss from URL and creates an rss::Channel
+    let search_terms = search_terms();
 
+
+    let channel = match Channel::from_url("http://feeds.washingtonpost.com/rss/rss_morning-mix") {
+        Ok(val) => val,
+        Err(_) => panic!("No data"),
+    };
+
+    let mut items: Vec<&Item> = vec![];
+
+    for item in channel.items().iter() {
+        if let Some(description) = item.description() {
+            for term in &search_terms {
+                if description.to_uppercase().as_str().contains(term) {
+                    items.push(&item);
+                }
+            }
+        }
+    }
+
+    // println!("{:?}", items);
+    for item in items {
+        println!("Title: {:?}", item.title());
+    }
+
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_something() {
+        let search_terms = search_terms();
+        assert!(search_terms.len() > 0);
+    }
 }
